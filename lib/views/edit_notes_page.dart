6bb7298fc9d63dc63839851a1040463e8ea9de12
Modern <fadysameh10@gmail.com>
2023-09-10 18:47:45 +0300

    create edit notes view

---
+++ lib/views/edit_notes_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_app_bar.dart';
+
+class EditNotePage extends StatelessWidget {
+  const EditNotePage({super.key});
+  static String id = 'Edit Note Page';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 40,
+            ),
+            CustomAppBar(),
+          ],
+        ),
+      ),
+    );
+  }
+}
