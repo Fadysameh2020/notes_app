d4931a169c837489b26698e14ccb1dde25de81c8
Modern <fadysameh10@gmail.com>
2023-09-16 15:05:22 +0300

    display data

--- lib/views/notes_page.dart
+++ lib/views/notes_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/views/widgets/add_note_bottom_sheet.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
 import 'package:notes_app/views/widgets/notes_list_view.dart';
@@ -8,36 +10,39 @@ class NotesPage extends StatelessWidget {
   static String id = 'Notes Page';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
-            ),
-            context: context,
-            builder: (context) {
-              return const AddNoteBottomSheer();
-            },
-          );
-        },
-      ),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 40,
-            ),
-            CustomAppBar(
-              icon: Icons.search,
-              title: 'Notes',
-            ),
-            Expanded(child: NotesListView()),
-          ],
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              context: context,
+              builder: (context) {
+                return const AddNoteBottomSheer();
+              },
+            );
+          },
+        ),
+        body: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 40,
+              ),
+              CustomAppBar(
+                icon: Icons.search,
+                title: 'Notes',
+              ),
+              Expanded(child: NotesListView()),
+            ],
+          ),
         ),
       ),
     );
